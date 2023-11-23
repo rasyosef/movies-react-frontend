d99b9fac6b61e17d7031d717c5f5f062e5e9f816
Yosef <yosefworku67@gmail.com>
2023-11-23 03:40:50 +0300

    added Home and Navbar components

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Movies</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">Add Movie</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
